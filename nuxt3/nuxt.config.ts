3071a4ec5e0ca2587292b1e8a1341176e1a915f7
andriawang6 <andria@gatech.edu>
2024-04-07 00:53:54 -0400

    Update nuxt.config.ts

--- nuxt3/nuxt.config.ts
+++ nuxt3/nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+// export default defineNuxtConfig({
 
-})
+// })
+export default {
+    build: {
+      // Specify the output directory
+      dir: 'docs/.nuxt/dist'
+    }
+  }
