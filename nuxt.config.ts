41d8390dd459ec48c8fa67aeb5f251c644f60cf2
andriawang6 <andria@gatech.edu>
2024-04-07 11:02:18 -0400

    Revert to changed project structure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 // export default defineNuxtConfig({
 
 // })
-module.exports = {
-  publicPath: '/daily-calendar/'
-}
+export default {
+    build: {
+      // Specify the output directory
+      dir: 'docs/.nuxt/dist'
+    }
+  }
